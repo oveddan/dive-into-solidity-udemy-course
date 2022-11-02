9525109c1adfb10d760b73595a78a64691ed7d68
Dan Oved <stangogh@gmail.com>
2022-11-02 14:33:42 -0400

    completed knowledge test

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,47 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+    modifier onlyOwner {
+        require(msg.sender == owner, "ONLY_OWNER");
+
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
+        tokens[0] = "VET";
+    }
+
+
+    receive() external payable {
+
+    }
+
+
+    fallback() external payable {
+
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+
+    function transferAll (address payable to) public onlyOwner {
+        to.transfer(address(this).balance);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+
+    function concatenate(string calldata a, string calldata b) external pure returns(string memory) {    
+        return string(abi.encodePacked(a, b));
     }
 }
